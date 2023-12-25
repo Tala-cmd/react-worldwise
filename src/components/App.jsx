2bc7410a731c2fa353b97a152ad3dc1ef7ff21b1
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-12-25 11:50:03 +0300

    building the Pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Homepage from "../pages/Homepage"
 import Pricing from "../pages/Pricing"
 import PageNotFound from "../pages/PageNotFound"
 import AppLayout from "../pages/AppLayout"
+import Login from '../pages/Login'
 
 function App() {
 
@@ -11,8 +12,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='product' element= {<Product />} />
         <Route path='pricing' element= {<Pricing />} />
+        <Route path='product' element= {<Product />} />
+        <Route path='/login' element= {<Login />} />
         <Route path='app' element= {<AppLayout />} />
         <Route path="*" element= {<PageNotFound />} />
       </Routes>
