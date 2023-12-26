3fdcc427dc113c9b0df42659b4081c4acd604e5f
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-12-26 23:15:13 +0300

    Implementing the cities list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,54 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Product from '../pages/Product'
-import Homepage from "../pages/Homepage"
-import Pricing from "../pages/Pricing"
-import PageNotFound from "../pages/PageNotFound"
-import AppLayout from "../pages/AppLayout"
-import Login from '../pages/Login'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "../pages/Product";
+import Homepage from "../pages/Homepage";
+import Pricing from "../pages/Pricing";
+import PageNotFound from "../pages/PageNotFound";
+import AppLayout from "../pages/AppLayout";
+import Login from "../pages/Login";
+import CityList from "./CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  return(
+  useEffect(function () {
+    async function fetchCities() {
+        try {
+          setIsLoading(true)
+          const res = await fetch(`${BASE_URL}/cities`);
+          const data = await res.json();
+          setCities(data);
+        } catch {
+          alert("There was an error loading data...");
+        } finally {
+          setIsLoading(false)
+        }
+    }
+    fetchCities();
+  }, []);
+
+  return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path='pricing' element= {<Pricing />} />
-        <Route path='product' element= {<Product />} />
-        <Route path='/login' element= {<Login />} />
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route path='app' element= {<AppLayout />}>
-          <Route index element={<p>LIST OF CITIES</p>} />
-          <Route path="cities" element= {<p>LIST OF CITIES</p>} />
-          <Route path="countries" element= {<p>COUNTRIES</p>} />
-          <Route path="form" element= {<p>FORM</p>} />
-        </Route>
+          <Route path="app" element={<AppLayout />}>
+              <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+              <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+              <Route path="countries" element={<p>COUNTRIES</p>} />
+              <Route path="form" element={<p>FORM</p>} />
+          </Route>
 
-        <Route path="*" element= {<PageNotFound />} />
-      </Routes>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
+import styles from './Sidebar.module.css'
+
 import { Outlet } from 'react-router-dom'
 import AppNav from './AppNav'
 import Logo from './Logo'
-import styles from './Sidebar.module.css'
 
 function Sidebar() {
   return (
