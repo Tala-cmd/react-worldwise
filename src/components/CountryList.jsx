cbad4434440f653136a7bf82fcba255488258294
Tala <70854429+Tala-cmd@users.noreply.github.com>
2024-01-01 21:19:32 +0300

    consuming CitiesContext

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -4,8 +4,11 @@ import styles from './CountryList.module.css'
 import CountryItem from './CountryItem'
 import Spinner from './Spinner'
 import Message from './Message'
+import { useCities } from '../contexts/CitiesContext';
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const {cities, isLoading} = useCities();
+
   if (isLoading) return <Spinner />
 
   if(!cities.length) return <Message message={'Add your first city by clicking on a city on the map'} />
