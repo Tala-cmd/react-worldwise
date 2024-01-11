931dbb9c5dd0c536d6df82edfe8bee3641b4d628
Tala <70854429+Tala-cmd@users.noreply.github.com>
2024-01-11 12:19:21 +0300

    using Callback hook in dependency array

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useContext, useReducer } from "react";
+import { createContext, useEffect, useContext, useReducer, useCallback } from "react";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -62,7 +62,7 @@ function CitiesProvider({ children }){
     fetchCities();
   }, []);
 
-  async function getCity(id){
+  const getCity = useCallback( async function getCity(id){
     if(Number(id) === currentCity.id) return;
 
     dispatch({ type: 'loading' })
@@ -73,7 +73,7 @@ function CitiesProvider({ children }){
     } catch {
       dispatch({ type: 'rejected', payload: 'There was an error loading city...'})
     }
-  }
+  }, [currentCity.id])
 
   async function createCity(newCity){
     dispatch({ type: 'loading' })
