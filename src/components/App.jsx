2be122daea6d9aa8a27ee4317cc999cea36299ef
Tala <70854429+Tala-cmd@users.noreply.github.com>
2024-01-11 15:46:01 +0300

    styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,26 +11,12 @@ import City from "./City";
 import Form from "./Form";
 import SpinnerFullPage from "./SpinnerFullPage";
 
-// import Product from "../pages/Product";
-// import Homepage from "../pages/Homepage";
-// import Pricing from "../pages/Pricing";
-// import PageNotFound from "../pages/PageNotFound";
-// import AppLayout from "../pages/AppLayout";
-// import Login from "../pages/Login";
-
-
 const Homepage = lazy(()=> import('../pages/Homepage'))
 const Product = lazy(()=> import('../pages/Product'))
 const Pricing = lazy(()=> import('../pages/Pricing'))
 const Login = lazy(()=> import('../pages/Login'))
 const AppLayout = lazy(()=> import('../pages/AppLayout'))
 const PageNotFound = lazy(()=> import('../pages/PageNotFound'))
-
-// Before using bundle 
-// dist/assets/index-DpLTSLv6.css   47.50 kB │ gzip:  11.51 kB
-// dist/assets/index-aG-R6P7X.js   533.31 kB │ gzip: 150.74 kB
-
-//const BASE_URL = "http://localhost:8000";
 
 function App() {
   
